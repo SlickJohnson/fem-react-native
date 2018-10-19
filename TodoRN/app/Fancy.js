9e030511eff0b5242cfb228f0382e6bf5dd36cfe
SlickJohnson <liwa.johnson@gmail.com>
2018-10-18 18:24:50 -0700

    Add a view with styles

--- TodoRN/app/Fancy.js
+++ TodoRN/app/Fancy.js
@@ -3,13 +3,19 @@ import {
   StyleSheet,
   View,
   Text,
+  Dimensions,
 } from 'react-native';
+
+const {width, height} = Dimensions.get('window');
 
 export class Fancy extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Fancy</Text>
+        <View style={styles.box}>
+
+        </View>
+        <Text style={styles.text}>Fancy</Text>
       </View>
     );
   }
@@ -18,6 +24,23 @@ export class Fancy extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ef235f'
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ef235f',
+    padding: 40,
+  },
+  box: {
+    backgroundColor: 'yellow',
+    width: width / 2,
+    height: height / 2,
+    position: 'absolute',
+    top: 20,
+    left: 30,
+    borderRadius: 30,
+  },
+  text: {
+    color: 'white',
+    fontSize: 34,
+    fontWeight: 'bold'
   }
 });
